973d49fc47f91fccb741ddfd61b3f0f63a3affcb
Yuu <trongdung.klnd@gmail.com>
2016-08-13 11:03:59 +0700

    Add layout

---
+++ app/src/main/java/com/example/yuu/socialinnovationcamp/MainScreen.java
@@ -0,0 +1,13 @@
+package com.example.yuu.socialinnovationcamp;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_screen);
+    }
+}
