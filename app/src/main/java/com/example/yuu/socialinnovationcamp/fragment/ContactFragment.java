d888d162edb8a83263ec942c3a8b86f9ca252bb2
Yuu <trongdung.klnd@gmail.com>
2016-08-14 11:36:39 +0700

    Add contact

---
+++ app/src/main/java/com/example/yuu/socialinnovationcamp/fragment/ContactFragment.java
@@ -0,0 +1,27 @@
+package com.example.yuu.socialinnovationcamp.fragment;
+
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.yuu.socialinnovationcamp.R;
+
+public class ContactFragment extends Fragment {
+
+
+    public ContactFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_contact, container, false);
+    }
+
+}
